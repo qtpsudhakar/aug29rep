03bf9222e8b859c191fc7de5abe57653bf295cb1
qtpsudhakar <sudhakar@qtpsudhakar>
2017-10-13 10:14:30 +0530

    demo1 changed

--- src/test/java/com/tng/ohrm/Demo1.java
+++ src/test/java/com/tng/ohrm/Demo1.java
@@ -5,6 +5,7 @@ public class Demo1 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("demo1 added");
+		System.out.println("new change1");
 	}
 
 }
